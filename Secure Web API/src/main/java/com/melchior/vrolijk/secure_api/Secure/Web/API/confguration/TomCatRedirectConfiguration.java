fd8989c25594c8d7540c906175b44e03f7165350
mel182 <42454027+mel182@users.noreply.github.com>
2019-04-04 16:23:29 +0200

    Http to https redirecting configuration feature added

---
+++ Secure Web API/src/main/java/com/melchior/vrolijk/secure_api/Secure/Web/API/confguration/TomCatRedirectConfiguration.java
@@ -0,0 +1,42 @@
+package com.melchior.vrolijk.secure_api.Secure.Web.API.confguration;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomCatRedirectConfiguration
+{
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcatServer = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        tomcatServer.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcatServer;
+    }
+
+    private Connector initiateHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+
+        return connector;
+    }
+}
--- Secure Web API/src/main/java/com/melchior/vrolijk/secure_api/Secure/Web/API/controller/PostController.java
+++ Secure Web API/src/main/java/com/melchior/vrolijk/secure_api/Secure/Web/API/controller/PostController.java
@@ -1,9 +1,25 @@
 package com.melchior.vrolijk.secure_api.Secure.Web.API.controller;
 
+import com.melchior.vrolijk.secure_api.Secure.Web.API.database.dbEnum.Category;
+import com.melchior.vrolijk.secure_api.Secure.Web.API.model.Post;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/post")
 @RestController
 public class PostController {
 
+    @GetMapping("/all")
+    public Post getAllPost()
+    {
+        Post post = new Post();
+        post.setTitle("Test title");
+        post.setDescription("This is a test description");
+        post.setCategory(Category.EDUCATIONAL);
+        post.setLastUpdate(System.currentTimeMillis());
+        post.setPublishedDate(System.currentTimeMillis());
 
+        return post;
+    }
 }
